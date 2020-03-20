96fc9c012212d1e8aaee8b63d78e64b122df4b8e
trannga2097 <62233335+trannga2097@users.noreply.github.com>
2020-03-20 12:45:15 +0700

    Create header.php

---
+++ job_portal/admin/include/header.php
@@ -0,0 +1,71 @@
+<?php
+
+    include('connection/db.php'); 
+
+    session_start();
+
+    if ($_SESSION['admin_id']==true) {
+    
+    }else{
+
+    header('location:admin_login.php');
+
+    }
+
+?>
+
+<!doctype html>
+
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <meta name="description" content="">
+
+    <meta name="author" content="">
+
+    <link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico">
+
+    <title>Dashboard Template for Bootstrap</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.min.css">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+
+    <link href="css/dashboard.css" rel="stylesheet">
+
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
+
+      <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Company name</a>
+
+      <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
+
+      <ul class="navbar-nav px-3">
+
+        <li class="nav-item text-nowrap">
+
+          <a class="nav-link" href="logout.php">Sign out</a>
+
+        </li>
+
+      </ul>
+
+    </nav>
+
+   
