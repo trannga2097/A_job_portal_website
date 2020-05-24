dfcf7bb73a2223825e9aa8cc16653bd2ee37d4de
Thunguyen1201 <62233688+Thunguyen1201@users.noreply.github.com>
2020-05-24 14:23:36 +0700

    Update sidebar.php

--- job_portal/admin/include/sidebar.php
+++ job_portal/admin/include/sidebar.php
@@ -1,3 +1,97 @@
+<!-- Nếu admin_type = 1 thì sẽ điều khiển được nhiều chức năng hơn admin_type = 2 -->
+<?php 
+
+  include('connection/db.php');
+
+  $sql = "SELECT * FROM `admin_login` WHERE `admin_email`='{$_SESSION['admin_email']}' and `admin_type`='Super Admin'";
+    
+  $result = mysqli_query($conn, $sql);
+    
+    if(mysqli_num_rows($result)>0){
+ 
+?>
+   <div class="container-fluid">
+
+      <div class="row">
+
+        <nav class="col-md-2 d-none d-md-block bg-light sidebar">
+
+          <div class="sidebar-sticky">
+
+            <ul class="nav flex-column">
+
+              <li class="nav-item">
+
+                <a class="nav-link active" href="admin_dashboard.php">
+
+                  <span data-feather="home"></span>
+
+                  Dashboard <span class="sr-only">(current)</span>
+
+                </a>
+
+              </li>
+
+              <li class="nav-item">
+
+                <a class="nav-link" href="admin.php">
+
+                  <span data-feather="users"></span>
+
+                  Admin
+
+                </a>
+
+              </li>
+
+              <li class="nav-item">
+
+                <a class="nav-link" href="create_company.php">
+
+                  <span data-feather="layers"></span>
+
+                  Company
+
+                </a>
+
+              </li>
+
+              <li class="nav-item">
+
+                <a class="nav-link" href="jobseeker.php">
+
+                  <span data-feather="layers"></span>
+
+                  Job Seeker
+
+                </a>
+
+              </li>
+
+            </ul>
+
+            <ul class="nav flex-column mb-2">
+
+              <li class="nav-item">
+
+                <a class="nav-link" href="category.php">
+
+                  <span data-feather="file-text"></span>
+
+                  Category
+
+                </a>
+
+              </li>
+
+            </ul>
+
+          </div>
+
+        </nav> 
+<?php
+}else{
+  ?>
     <div class="container-fluid">
 
       <div class="row">
@@ -22,47 +116,47 @@
 
               <li class="nav-item">
 
-                <a class="nav-link" href="#">
+                <!-- <a class="nav-link" href="#">
 
                   <span data-feather="file"></span>
 
                   Orders
 
-                </a>
+                </a> -->
 
               </li>
 
               <li class="nav-item">
 
-                <a class="nav-link" href="#">
+                <!-- <a class="nav-link" href="#">
 
                   <span data-feather="shopping-cart"></span>
 
                   Products
 
-                </a>
+                </a> -->
 
               </li>
 
               <li class="nav-item">
 
-                <a class="nav-link" href="customers.php">
+                <!-- <a class="nav-link" href="customers.php">
 
                   <span data-feather="users"></span>
 
                   Customers
 
-                </a>
+                </a> -->
 
               </li>
 
               <li class="nav-item">
 
-                <a class="nav-link" href="#">
+                <a class="nav-link" href="job_create.php">
 
                   <span data-feather="bar-chart-2"></span>
 
-                  Reports
+                  Job Create
 
                 </a>
 
@@ -70,29 +164,17 @@
 
               <li class="nav-item">
 
-                <a class="nav-link" href="#">
+                <a class="nav-link" href="apply_jobs.php">
 
                   <span data-feather="layers"></span>
 
-                  Integrations
+                  Apply Jobs
 
                 </a>
 
               </li>
 
             </ul>
-
-            <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-
-              <span>Saved reports</span>
-
-              <a class="d-flex align-items-center text-muted" href="#">
-
-                <span data-feather="plus-circle"></span>
-
-              </a>
-
-            </h6>
 
             <ul class="nav flex-column mb-2">
 
@@ -102,43 +184,7 @@
 
                   <span data-feather="file-text"></span>
 
-                  Current month
-
-                </a>
-
-              </li>
-
-              <li class="nav-item">
-
-                <a class="nav-link" href="#">
-
-                  <span data-feather="file-text"></span>
-
-                  Last quarter
-
-                </a>
-
-              </li>
-
-              <li class="nav-item">
-
-                <a class="nav-link" href="#">
-
-                  <span data-feather="file-text"></span>
-
-                  Social engagement
-
-                </a>
-
-              </li>
-
-              <li class="nav-item">
-
-                <a class="nav-link" href="#">
-
-                  <span data-feather="file-text"></span>
-
-                  Year-end sale
+                  Category
 
                 </a>
 
@@ -149,3 +195,6 @@
           </div>
 
         </nav>
+<?php
+  }
+ ?>
